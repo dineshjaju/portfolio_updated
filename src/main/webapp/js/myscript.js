600ec4aac8157c937bf71b72fa16609a37ebeeb0
dineshjaju <dineshkumarjaju@gmail.com>
2016-04-19 17:42:24 -0400

    Final version - first update

---
+++ src/main/webapp/js/myscript.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+   
+   
+
+    /* Scroll spy and scroll filter */
+    $('#main-menu').onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollThreshold: 0.5,
+        scrollSpeed: 750,
+        filter: "",
+        easing: "swing"
+    });
+
+
+    /* Scroll hire me button to contact page */
+    $('.hire-me').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
+    });
+
+        /* Charts*/
+
+    $('.chart').waypoint(function() {
+        $(this).easyPieChart({
+            barColor: '#3498db',
+            size: '150',
+            easing: 'easeOutBounce',
+            onStep: function(from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent));
+            }
+        });
+    }, {
+        triggerOnce: true,
+        offset: 'bottom-in-view'
+    });
+
+
+
+});
