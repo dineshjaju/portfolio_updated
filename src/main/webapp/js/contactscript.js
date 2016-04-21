1d274ecccb81982157e57a6376a6152e385a9db2
dinesh.jaju <dineshkumarjaju@gmail.com>
2016-04-21 11:29:50 -0400

    mail and form validations

---
+++ src/main/webapp/js/contactscript.js
@@ -0,0 +1,26 @@
+function validateForm(argument) 
+{
+	var name = document.forms["contactForm"]["name"].value;
+	var emailId = document.forms["contactForm"]["email"].value;
+	var message = document.forms["contactForm"]["message"].value;
+
+	alert(name + emailId + message);
+
+	$.ajax(
+		{
+			url:"mail/contact_me.php",
+			type: "POST",
+			data: {name: name, email: emailId, message: message},
+			cache: false,
+			success : function()
+			{
+				alert('Message sent successfully');
+			},
+			error : function()
+			{
+				alert('Message failed please drop mail directly to dineshkumaraju');
+			}
+		}
+
+	);
+}
