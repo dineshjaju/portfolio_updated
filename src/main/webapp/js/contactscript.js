27ee12dea8ad59e96166be42e52bb82a81a1e969
dinesh.jaju <dineshkumarjaju@gmail.com>
2016-04-21 11:40:43 -0400

    fix success

--- src/main/webapp/js/contactscript.js
+++ src/main/webapp/js/contactscript.js
@@ -15,7 +15,13 @@ function validateForm(argument)
 			success : function()
 			{
 				alert('Message sent successfully');
-				$('#success').html("<h1>Message sent successfully</h1>")
+				 $('#success').html("<div class='alert alert-success'>");
+            	  $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
+            	  $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+ 		  		 $('#success > .alert-success').append('</div>');
+				
+				//clear all fields
+ 		  		$('#contactForm').trigger("reset");
 			},
 			error : function()
 			{
