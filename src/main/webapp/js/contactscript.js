f1cd0235ff83663a03c92574cd7c779d5c56843e
dinesh.jaju <dineshkumarjaju@gmail.com>
2016-04-21 13:14:42 -0400

    update link color

--- src/main/webapp/js/contactscript.js
+++ src/main/webapp/js/contactscript.js
@@ -14,7 +14,7 @@ function validateForm(argument)
 
 	$.ajax(
 		{
-			url:"mail/contact_me.php1",
+			url:"mail/contact_me.php",
 			type: "POST",
 			data: {name: name, email: emailId, message: message},
 			cache: false,
@@ -41,7 +41,7 @@ function validateForm(argument)
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
             	$('#success > .alert-danger').append("<strong>Sorry "+name+" it seems that my mail server is not responding...</strong> Could you please email me directly to ");
-            	$('#success > .alert-danger').append(" <a href='mailto:dineshkumaraju@gmail.com' style='display:none;';>dineshkumaraju@gmail.com</a>. Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append(" <a href='mailto:dineshkumaraju@gmail.com' style='color:red;'>dineshkumaraju@gmail.com</a>. Sorry for the inconvenience!");
  	        	$('#success > .alert-danger').append('</div>'); 				
  				//clear all fields
  				$('#contactForm').trigger("reset");
