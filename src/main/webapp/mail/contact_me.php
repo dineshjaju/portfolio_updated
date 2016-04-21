7867578a857494ee520573e9c9a565cec7f664e4
dinesh.jaju <dineshkumarjaju@gmail.com>
2016-04-21 17:11:46 -0400

    mail setup

--- src/main/webapp/mail/contact_me.php
+++ src/main/webapp/mail/contact_me.php
@@ -1,5 +1,8 @@
 <?php
-// check if fields passed are empty
+
+require_once 'lib/swift_required.php';
+
+/*// check if fields passed are empty
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
@@ -22,6 +25,23 @@ $email_body = "You have received a new message. \n\n".
 $headers = "From: noreply@yourdomain.com\n"; 
 // Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+mail($to,$email_subject,$email_body,$headers);*/
+		
+
+// make sure you get these SMTP settings right
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl") 
+    ->setUsername('dineshkumarjaju@gmail.com')
+    ->setPassword('pc2727pcds');
+
+$mailer = Swift_Mailer::newInstance($transport);
+// the message itself
+$message = Swift_Message::newInstance('email subject')
+    ->setFrom(array('dineshkumarjaju@gmail.com' => 'no reply'))
+    ->setTo(array('dineshkumarjaju@gmail.com'))
+    ->setBody("email body");
+
+$result = $mailer->send($message);
+
+return true;
+
 ?>
