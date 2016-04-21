8cc57661bfb09efa7a127b4e14204903413e998e
dinesh.jaju <dinesh.jaju@L009797.rci.rogers.ca>
2016-04-21 19:21:47 -0400

    update contact_me

--- src/main/webapp/mail/contact_me.php
+++ src/main/webapp/mail/contact_me.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once 'lib/swift_required.php'; 
+//require_once 'lib/swift_required.php'; 
 
-/*// check if fields passed are empty
+// check if fields passed are empty
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		|| 
    empty($_POST['message'])	||
@@ -17,18 +17,18 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'dineshkumarjaju@gmail.com'; // put your email address here
+$to = 'support@dineshjaju.com'; // put your email address here
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message: \n $message";
-$headers = "From: noreply@yourdomain.com\n"; 
+$headers = "From: noreply@dineshjaju.com\n"; 
 // Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);*/
+mail($to,$email_subject,$email_body,$headers);
 		
 
-// make sure you get these SMTP settings right
+/*// make sure you get these SMTP settings right
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, "tls") 
     ->setUsername('dineshkumarjaju@gmail.com')
     ->setPassword('');
@@ -41,7 +41,7 @@ $message = Swift_Message::newInstance('email subject')
     ->setBody("email body");
 
 $result = $mailer->send($message);
-
+*/
 return true;
 
 ?>
