424e6158ab7081f0a42fa6ad3c1e246b9ca66f49
dinesh.jaju <dineshkumarjaju@gmail.com>
2016-05-12 14:23:04 -0400

    mails

--- src/main/java/com/portfolio/configuration/OrderManagementConfiguration.java
+++ src/main/java/com/portfolio/configuration/OrderManagementConfiguration.java
@@ -3,11 +3,24 @@ package com.portfolio.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.portfolio")
 public class OrderManagementConfiguration extends WebMvcConfigurerAdapter{
 	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/");
+		viewResolver.setSuffix(".html");
+		registry.viewResolver(viewResolver);
+	}
+
 }
