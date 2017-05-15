ddf33924157ede3bcf4f9b220a2dcf680686235a
Owner <Owner@DESKTOP-88A10RB>
2017-05-15 13:03:44 -0500

    fix issue with style and java script

--- src/main/webapp/js/contactscript.js
+++ src/main/webapp/js/contactscript.js
@@ -41,7 +41,7 @@ function validateForm(argument)
 					$('#success').html("<div class='alert alert-danger'>");
 					$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
 					.append( "</button>");
-					$('#success > .alert-danger').append("<strong>Sorry "+name+" it seems that my mail server is not responding...</strong> Could you please email me directly to ");
+					$('#success > .alert-danger').append("<strong>Sorry "+name+" it seems that mail server is not responding...</strong> Could you please email me directly to ");
 					$('#success > .alert-danger').append(" <a href='mailto:dineshkumaraju@gmail.com' style='color:red;'>dineshkumarjaju@gmail.com</a>. Sorry for the inconvenience!");
 					$('#success > .alert-danger').append('</div>'); 				
 					//clear all fields
