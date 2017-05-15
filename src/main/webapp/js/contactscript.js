55d0f23b4b835f5fbbb3f99901aa7cf747088415
Owner <Owner@DESKTOP-88A10RB>
2017-05-15 13:10:49 -0500

    fix email issue

--- src/main/webapp/js/contactscript.js
+++ src/main/webapp/js/contactscript.js
@@ -42,7 +42,7 @@ function validateForm(argument)
 					$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
 					.append( "</button>");
 					$('#success > .alert-danger').append("<strong>Sorry "+name+" it seems that mail server is not responding...</strong> Could you please email me directly to ");
-					$('#success > .alert-danger').append(" <a href='mailto:dineshkumaraju@gmail.com' style='color:red;'>dineshkumarjaju@gmail.com</a>. Sorry for the inconvenience!");
+					$('#success > .alert-danger').append(" <a href='mailto:dineshkumarjaju@gmail.com' style='color:red;'>dineshkumarjaju@gmail.com</a>. Sorry for the inconvenience!");
 					$('#success > .alert-danger').append('</div>'); 				
 					//clear all fields
 					$('#contactForm').trigger("reset");
