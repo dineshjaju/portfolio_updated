0780a6a35c9d74f0b3630c53384ff937ebfbcc2b
poojithanagandla <42778602+poojithanagandla@users.noreply.github.com>
2018-10-05 09:51:24 -0400

    Update Experience

--- src/main/java/com/portfolio/mail/SimpleMailServlet.java
+++ src/main/java/com/portfolio/mail/SimpleMailServlet.java
@@ -13,41 +13,34 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
-public class SimpleMailServlet extends HttpServlet 
-{
+public class SimpleMailServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -250900940224932079L;
-	 
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
- 
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		String name = req.getParameter("name");
-		String emailId =  req.getParameter("emailId");
+		String emailId = req.getParameter("emailId");
 		String message = req.getParameter("message");
 
 		SendSimpleMessage(name, emailId, message);
 	}
 
-	public  String SendSimpleMessage(String name, String emailId, String message) 
-	{
-		name = name.replaceAll("\\s+","");
-		
-		Client client = Client.create();
-		client.addFilter(new HTTPBasicAuthFilter("api", 
-				"key-61f5c5ad90ed0752759c11abf3231944"));
-		WebResource webResource =
-				client.resource("https://api.mailgun.net/v3/www.dineshjaju.com" +
-						"/messages");
-		MultivaluedMapImpl formData = new MultivaluedMapImpl();
-		formData.add("from", name +" PortFolio Contact  <mailgun@" + name + ".com>");
-		formData.add("to", "dineshkumarjaju@gmail.com");
-		//formData.add("to", "YOU@YOUR_DOMAIN_NAME");
-		formData.add("subject", "Portfolio Mail");
-		formData.add("text", "Email Recieved from " + emailId + " with following message  "  + message);
-		webResource.type(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED).
-		post(ClientResponse.class, formData);
+	public String SendSimpleMessage(String name, String emailId, String message) {
+		name = name.replaceAll("\\s+", "");
 
-		return "success " ;
+		Client client = Client.create();
+		client.addFilter(new HTTPBasicAuthFilter("api", "key-61f5c5ad90ed0752759c11abf3231944"));
+		WebResource webResource = client.resource("https://api.mailgun.net/v3/www.dineshjaju.com" + "/messages");
+		MultivaluedMapImpl formData = new MultivaluedMapImpl();
+		formData.add("from", name + " PortFolio Contact  <mailgun@" + name + ".com>");
+		formData.add("to", "dineshkumarjaju@gmail.com");
+		// formData.add("to", "YOU@YOUR_DOMAIN_NAME");
+		formData.add("subject", "Portfolio Mail");
+		formData.add("text", "Email Recieved from " + emailId + " with following message  " + message);
+		webResource.type(javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED).post(ClientResponse.class, formData);
+
+		return "success ";
 	}
 }
